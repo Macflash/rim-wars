52aab0035ae274554733daeeb45acd2bc66597d1
Alex Breyfogle <alexbr@microsoft.com>
2019-02-20 07:57:08 -0800

    Add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,107 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+enum Tile {
+  Empty,
+  Wall,
+  Team1,
+  Team2
+}
+
+interface IAppState {
+  terrainCanvas?: HTMLCanvasElement;
+  playerCanvas?: HTMLCanvasElement;
+}
+
+class App extends Component<{}, IAppState> {
+  private size = 600;
+  private tiles = 20;
+  private terrainCtx?: CanvasRenderingContext2D;
+  private playerCtx?: CanvasRenderingContext2D;
+
+  constructor(props: any) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidUpdate(){
+    if(this.playerCtx && this.terrainCtx){
+      const map = this.createEmptyMap(this.tiles);
+      map[1][1] = Tile.Wall;
+      map[3][5] = Tile.Team1;
+      map[18][13] = Tile.Team2;
+      this.drawTerrain(map, this.terrainCtx);
+    }
+  }
+
+  private createEmptyMap(tiles: number): Tile[][] {
+    let map: any[][] = [];
+    for (let x = 0; x < tiles; x++) {
+      map[x] = [];
+      for (let y = 0; y < tiles; y++) {
+        map[x][y] = Tile.Empty;
+      }
+    }
+
+    return map;
+  }
+
+  private drawTerrain(map: Tile[][], ctx: CanvasRenderingContext2D) {
+    const tileSize = Math.max(Math.floor(this.size / this.tiles), 1);
+    for (let x = 0; x < map.length; x++) {
+      for (let y = 0; y < map[x].length; y++) {
+        switch (map[x][y]) {
+          case Tile.Empty:
+            ctx.fillStyle = "white";
+            break;
+          case Tile.Wall:
+            ctx.fillStyle = "black";
+            break;
+          case Tile.Team1:
+            ctx.fillStyle = "red";
+            break;
+          case Tile.Team2:
+            ctx.fillStyle = "blue";
+            break;
+
+        }
+
+        ctx.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
+      }
+    }
+  }
+
+  private setPlayerCanvas = (playerCanvas: HTMLCanvasElement | null) => {
+    if (playerCanvas && !this.state.playerCanvas) {
+      this.setState({ playerCanvas });
+      this.playerCtx = playerCanvas.getContext("2d")!;
+    }
+  }
+
+  private setTerrainCanvas = (terrainCanvas: HTMLCanvasElement | null) => {
+    if (terrainCanvas && !this.state.terrainCanvas) {
+      this.setState({ terrainCanvas });
+      this.terrainCtx = terrainCanvas.getContext("2d")!;
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <canvas
+          style={{ position: "absolute", top: 0, left: 0, zIndex: 1 }}
+          height={this.size}
+          width={this.size}
+          id="terrainCanvas"
+          ref={this.setTerrainCanvas}
+        />
+        <canvas
+          style={{ position: "absolute", top: 0, left: 0, zIndex: 2 }}
+          height={this.size}
+          width={this.size}
+          id="playerCanvas"
+          ref={this.setPlayerCanvas}
+        />
       </div>
     );
   }
